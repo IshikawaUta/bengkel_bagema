91ebbb5562443a7f3476b0e2759f8c9d1871ea6b
Eka saputra <jayaj7129@gmail.com>
2025-10-24 02:07:40 +0000

    80px (_for element img_)

--- src/components/site/logo.tsx
+++ src/components/site/logo.tsx
@@ -20,8 +20,8 @@ export function Logo({ className, onClick }: LogoProps) {
       <Image 
         src="https://res.cloudinary.com/dzsqaauqn/image/upload/v1761271214/WhatsApp_Image_2025-10-21_at_20.03.01-removebg-preview_admdnw.png"
         alt="Bengkel Bagema Logo"
-        width={100}
-        height={22}
+        width={80}
+        height={18}
         priority
       />
     </Link>
