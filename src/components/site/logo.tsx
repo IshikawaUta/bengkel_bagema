59b13586e2ed617633d85508af7ea1537c7fba18
Eka saputra <jayaj7129@gmail.com>
2025-10-24 01:58:25 +0000

    ubah seperti semula (_for element img_)

--- src/components/site/logo.tsx
+++ src/components/site/logo.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import Link from "next/link";
+import { Wrench } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 type LogoProps = {
@@ -13,17 +13,12 @@ export function Logo({ className, onClick }: LogoProps) {
       href="/"
       onClick={onClick}
       className={cn(
-        "flex items-center gap-2",
+        "flex items-center gap-2 text-xl font-bold font-headline text-primary",
         className
       )}
     >
-      <Image 
-        src="https://res.cloudinary.com/dzsqaauqn/image/upload/v1761270951/WhatsApp_Image_2025-10-21_at_20.03.01_b8sec9.jpg" 
-        alt="Bengkel Bagema Logo" 
-        width={140} 
-        height={35}
-        className="object-contain"
-      />
+      <Wrench className="h-6 w-6" />
+      Bengkel Bagema
     </Link>
   );
 }
