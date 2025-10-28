0bd89ecfcfe369c871a63c5071a5c585f8392110
Eka saputra <jayaj7129@gmail.com>
2025-10-28 15:09:25 +0000

    Try fixing this error: `Console Error: params are being enumerated. `par

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -3,7 +3,6 @@
 import { Header } from "@/components/site/header";
 import { Footer } from "@/components/site/footer";
 import { PageTransition } from "@/components/site/page-transition";
-import { useEffect, useState } from "react";
 import { ScrollToTopButton } from "@/components/site/scroll-to-top-button";
 
 export default function SiteLayout({
@@ -11,30 +10,15 @@ export default function SiteLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
 
   return (
     <div className="relative flex min-h-screen flex-col">
-      {isClient ? (
-        <>
-          <PageTransition>
-            <Header />
-            <main className="flex-1">{children}</main>
-            <Footer />
-          </PageTransition>
-          <ScrollToTopButton />
-        </>
-      ) : (
-        <>
+        <PageTransition>
           <Header />
           <main className="flex-1">{children}</main>
           <Footer />
-        </>
-      )}
+        </PageTransition>
+        <ScrollToTopButton />
     </div>
   );
 }
